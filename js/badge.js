454a4a09d7f919e7527b6417da3ebec747075e7b
Cother <ihamsterball@gmail.com>
2024-12-11 01:21:52 +0800

    Add badges in console for WebAssembly module

--- js/badge.js
+++ js/badge.js
@@ -4,14 +4,22 @@ const badges = Object.freeze({
     Sector:        { name: 'Sector',         color: 'hsl(59, 70%, 60%)' },
     Firebase:      { name: 'Firebase',       color: 'hsl(71, 65%, 60%)' },
     ServiceWorker: { name: 'Service Worker', color: 'hsl(205, 100%, 65%)' },
-    WebRTC:        { name: 'WebRTC',         color: 'hsl(266, 60%, 75%)'},
+    WebRTC:        { name: 'WebRTC',         color: 'hsl(0, 60%, 50%)'},
+    WebAssembly:   { name: 'WebAssembly',    color: 'hsl(248, 60%, 50%)'},
 });
 
 class Badge {
     static args(...badges) {
         const text_color = '#FFFFFF';
         const names = badges.map(badge => `%c${badge.name}`);
-        const values = badges.map(badge => `background-color: ${badge.color}; color: ${text_color}; padding: 2px 4px; border-radius: 4px;`);
+        const values = badges.map(badge => [`background-color: ${badge.color}`,
+                                            `color: ${text_color}`,
+                                            `padding: 2px 4px`,
+                                            `border-radius: 4px`].join(';'));
         return Array.prototype.concat(names.join(''), values);
+    }
+
+    static wrap(target, badges) {
+        return function (...args) { return Function.prototype.bind.call(target, console, ...Badge.args(...badges), ...args) }();
     }
 }
